126762335e8b0304b51e98047d364c483290e052
JulianeGampe <juliane.gampe@gmail.com>
2022-05-29 17:59:40 +0000

    Create and register Course, Level, Format models

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Course, Level, Format
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'level',
+        'format',
+        'description',
+        'price',
+        'startdate',
+        'weekday',
+        'time',
+        'duration',
+        'status',
+    )
+
+    ordering = ('name',)
+
+class LevelAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendlyname',
+        'name',
+    )
+
+class FormatAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendlyname',
+        'name',
+    )
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Level, LevelAdmin)
+admin.site.register(Format, FormatAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS = ((0, "Bookable"), (1, "Booking closed"))
+
+
+class Level(models.Model):
+    name = models.CharField(max_length=254)
+    friendlyname = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendlyname(self):
+        return self.friendlyname
+
+
+class Format(models.Model):
+    name = models.CharField(max_length=254)
+    friendlyname = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendlyname(self):
+        return self.friendlyname
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=254)
+    level = models.ForeignKey('Level', null=True, blank=True, on_delete=models.SET_NULL)
+    format = models.ForeignKey('Format', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    startdate = models.DateField()
+    weekday = models.CharField(max_length=254)
+    time = models.TimeField()
+    duration = models.CharField(max_length=254)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.name
