c5337804f68fce7a07e85c5e7f1dd441a52321ae
JulianeGampe <juliane.gampe@gmail.com>
2022-06-02 08:03:45 +0000

    Create the coursedetail view

--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 
 def allcourses(request):
+    """
+    View to display all courses
+    """
     courses = Course.objects.all()
     template = 'courses/courses.html'
     context = {
         'courses': courses
+    }
+    return render(request, template, context)
+
+
+def coursedetail(request, course_id):
+    """
+    View to display details of one course
+    """
+    course = get_object_or_404(Course, pk=course_id)
+    template = 'courses/course_detail.html'
+    context = {
+        'course': course
     }
     return render(request, template, context)
