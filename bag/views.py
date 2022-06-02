4a9b9a6f4d36fc4f3303f350f57462d338ee0963
JulianeGampe <juliane.gampe@gmail.com>
2022-06-02 19:01:48 +0000

    Create the addbag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def viewbag(request):
@@ -7,3 +7,13 @@ def viewbag(request):
     """
     template = 'bag/bag.html'
     return render(request, template)
+
+
+def addbag(request, course_id):
+    """
+    Add the chosen course to the bag
+    """
+    redirecturl = request.POST.get('redirecturl')
+    bag = request.session.get('bag', {})
+    request.session['bag'] = bag
+    return redirect(redirecturl)
