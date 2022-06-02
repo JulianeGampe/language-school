8d829f7d117505f2cdabaee0ec9c2133195ab84d
JulianeGampe <juliane.gampe@gmail.com>
2022-06-02 17:42:56 +0000

    Create the viewbag view

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def viewbag(request):
+    """
+    View to render the shopping bag page
+    """
+    template = 'bag/bag.html'
+    return render(request, template)
