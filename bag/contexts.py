464593b375c2c4fe72cd731579c9292db3b694c6
JulianeGampe <juliane.gampe@gmail.com>
2022-06-02 19:55:51 +0000

    Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,26 @@
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 
 def bagcontents(request):
     bagitems = []
     total = 0
     productcount = 0
+    bag = request.session.get('bag', {})
+
+    for course_id, quantity in bag.items():
+        course = get_object_or_404(Course, pk=course_id)
+        total = quantity * course.price
+        productcount = quantity
+        bagitems.append({
+            'course_id': course_id,
+            'quantity': quantity,
+            'course': course,
+        })
+
     context = {
         'bagitems': bagitems,
         'total': total,
-        'productcount': productcount
+        'productcount': productcount,
     }
     return context
