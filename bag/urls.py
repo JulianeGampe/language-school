98b36b6906721cf8d2f6e4d912f733aee9f10643
JulianeGampe <juliane.gampe@gmail.com>
2022-06-03 13:15:55 +0000

    Add path for removebag

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.viewbag, name='viewbag'),
-    path('add/<course_id>/', views.addbag, name='addbag')
+    path('add/<course_id>/', views.addbag, name='addbag'),
+    path('remove/<course_id>/', views.removebag, name='removebag'),
 ]
