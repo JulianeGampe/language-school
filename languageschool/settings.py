944028dcf99bb08a180b0f2e4346a8cb9a4e760d
JulianeGampe <juliane.gampe@gmail.com>
2022-06-03 19:31:05 +0000

    Add stripe public key

--- languageschool/settings.py
+++ languageschool/settings.py
@@ -169,3 +169,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
