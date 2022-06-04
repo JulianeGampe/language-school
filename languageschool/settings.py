22eed4fb59ef6d3b1b1e0e84da1a610ee0ed416a
JulianeGampe <juliane.gampe@gmail.com>
2022-06-04 09:58:30 +0000

    Add stripe variables

--- languageschool/settings.py
+++ languageschool/settings.py
@@ -172,4 +172,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Stripe
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
