a8df54bc1160cc77bf1121e08e204f77aad9d057
JulianeGampe <juliane.gampe@gmail.com>
2022-06-05 12:47:25 +0000

    Add path for order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path(
+        'orderhistory/<ordernumber>', views.orderhistory, name='orderhistory'
+    ),
 ]
