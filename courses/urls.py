32aebbb38a131f4f832e41d832299cb2b868af19
JulianeGampe <juliane.gampe@gmail.com>
2022-06-06 09:01:24 +0000

    Add the url path for editcourse

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.allcourses, name='allcourses'),
     path('<int:course_id>/', views.coursedetail, name='coursedetail'),
     path('add/', views.addcourse, name='addcourse'),
+    path('edit/<int:course_id>', views.editcourse, name='editcourse'),
 ]
