abeaef21c1cef41c1f503359e0e26731f2828a66
JulianeGampe <juliane.gampe@gmail.com>
2022-06-06 07:47:10 +0000

    Add the path to add courses to urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.allcourses, name='allcourses'),
-    path('<int:course_id>/', views.coursedetail, name='coursedetail')
+    path('<int:course_id>/', views.coursedetail, name='coursedetail'),
+    path('add/', views.addcourse, name='addcourse'),
 ]
