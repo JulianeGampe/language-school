3669c89259fda5de27660a69ad0306aee7030358
JulianeGampe <juliane.gampe@gmail.com>
2022-06-08 13:37:00 +0000

    Remove webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .webhooks import webhook
+# from .webhooks import webhook
 
 
 urlpatterns = [
@@ -10,10 +10,4 @@ urlpatterns = [
         views.checkoutsuccess,
         name='checkoutsuccess'
     ),
-    path(
-        'cache_checkout_data/',
-        views.cache_checkout_data,
-        name='cache_checkout_data'
-    ),
-    path('wh/', webhook, name='webhook'),
 ]
--- languageschool/settings.py
+++ languageschool/settings.py
@@ -186,4 +186,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
