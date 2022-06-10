3aec849e32ecc9f583c46e8ee88dac7516d24d8e
JulianeGampe <juliane.gampe@gmail.com>
2022-06-10 19:28:15 +0000

    Amend the home view to handle newsletterform

--- home/views.py
+++ home/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .forms import NewsletterForm
+from .models import Newsletter
 
 
 def home(request):
+    """
+    View to render the homepage and
+    handle the post request of the NewsletterForm
+    """
+    if request.method == 'POST':
+        newsletterform = NewsletterForm(request.POST)
+        emails = Newsletter.objects.all()
+        emails = emails.values_list()
+        emaillist = [item[1] for item in emails]
+        if request.POST['email'] in emaillist:
+            messages.info(request, 'You are already subscribed')
+        else:
+            if newsletterform.is_valid():
+                newsletterform.save()
+                messages.success(
+                    request, 'Thank you for subscribing to our newsletter!'
+                )
+                return redirect('home')
+            else:
+                messages.error(
+                    request, 'Failed to send. Please check your email address'
+                )
+    else:
+        newsletterform = NewsletterForm()
+
     template = 'home/index.html'
     context = {
-
+        'newsletterform': newsletterform
     }
     return render(request, template, context)
