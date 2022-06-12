87ae4dbebbc2b6f628683b8446b13508759a5d82
JulianeGampe <juliane.gampe@gmail.com>
2022-06-12 19:09:43 +0000

    Change default from email

--- languageschool/settings.py
+++ languageschool/settings.py
@@ -200,4 +200,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER_DEFAULT')
+    DEFAULT_FROM_EMAIL = 'learngermanonline@example.com'
